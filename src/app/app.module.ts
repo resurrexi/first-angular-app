04853f7f2808aa20b4658460b378cf750b90631c
Liquan Yang <liquan.yang@torqata.com>
2024-03-14 09:59:00 -0400

    assignment 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,17 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { WarningComponent } from './warning/warning.component';
+import { SuccessComponent } from './success/success.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    ServersComponent,
+    WarningComponent,
+    SuccessComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servers', // by element
+  // selector: '[app-servers]', // by attribute
+  // selector: '.app-servers', // by class name
+  templateUrl: './servers.component.html',
+  styleUrl: './servers.component.css'
+})
+export class ServersComponent {
+
+}
